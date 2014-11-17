c050bc9af6b8e03d12cee1d13cacf67db6c9b035
Edgar Sanchez <edgar.sanchez@atrapalo.com>
2014-11-17 16:58:59 +0100

    First commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,30 @@
+<?php
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\CustomSnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+/**
+ * Defines application features from the specific context.
+ */
+class FeatureContext implements Context, CustomSnippetAcceptingContext
+{
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+    }
+
+
+    public static function getAcceptedSnippetType()
+    {
+        return 'regex';
+    }
+
+}
